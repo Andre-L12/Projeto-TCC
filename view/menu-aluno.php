4acf17747dba763aed513ddd7c0ae4e63cfa222d
andreluiz <dresiqueira12@gmail.com>
2024-09-27 09:21:06 -0300

    PIPIPOPO MUDANÇAS FEITAS NA AULA PIRU

---
+++ view/menu-aluno.php
@@ -0,0 +1,49 @@
+<!-- Toda página que puxar Navbar terá que ter essa base -->
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu</title>
+
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/remixicon@2.2.0/fonts/remixicon.css'>
+    <link rel='stylesheet' href='https://unpkg.com/css-pro-layout@1.1.0/dist/css/css-pro-layout.css'>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;display=swap'>
+    <link rel="stylesheet" href="./navbar-estilos.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
+
+</head>
+<body>
+    <div class="layout has-sidebar fixed-sidebar fixed-header">
+        <?php
+            require_once "navbar.html";
+        ?>
+
+        <div class="layout">
+        <header>
+            <div>
+                <span class="header-icon">
+                    <i class="bi bi-grid-fill"></i>
+                </span>
+                <span class="header-title">MENU</span>
+            </div>
+        </header>
+            <main class="content">
+                <div>
+                    <a id="btn-toggle" href="#" class="sidebar-toggler break-point-sm">
+                        <i class="ri-menu-line ri-xl"></i>
+                    </a>
+                </div>
+            </main>
+            <div class="overlay"></div>
+        </div>
+    </div>
+
+        <script src='https://unpkg.com/@popperjs/core@2'></script>
+    <script src="navbar-script.js"></script>
+</body>
+
+</html>
