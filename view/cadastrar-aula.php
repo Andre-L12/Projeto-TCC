50b2c207651785d81464b99e5ddb2096b7b8cd13
annaraquelsnd <annaraquelsandrini@gmail.com>
2024-10-03 15:52:37 -0300

    Novo Cadastrar Instrutor + usando navbar html

--- view/cadastrar-aula.php
+++ view/cadastrar-aula.php
@@ -20,9 +20,7 @@
 <body>
     <div class="layout has-sidebar fixed-sidebar fixed-header">
         <?php
-        require_once "navbar-chamar.php";
-        $navbar = chamarNavbar();
-        echo $navbar;
+        require_once "navbar.html";
         ?>
 
         <div class="layout">
