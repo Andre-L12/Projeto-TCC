69ac795e187d905606dc0cfee95999b78e3573f9
andreluiz <dresiqueira12@gmail.com>
2024-12-08 08:41:29 -0300

    conexão local

--- model/conexaoBD.php
+++ model/conexaoBD.php
@@ -1,10 +1,14 @@
 
 <?php
 function conectarBD(){
-    $host="bancocfc.cztq4u6ge5ld.us-east-1.rds.amazonaws.com";
-    $user="admin";
-    $senha="aev01062427";
-    $database="BANCO_CFC";
+    // $host="bancocfc.cztq4u6ge5ld.us-east-1.rds.amazonaws.com";
+    // $user="admin";
+    // $senha="aev01062427";
+    // $database="BANCO_CFC";
+     $host="127.0.0.1";
+     $user="root";
+     $senha="";
+     $database="banco_cfc";
 
     // conexao bd
     $conexao = mysqli_connect($host,$user,$senha,$database) or die("Erro ao conectar com o banco de dados.");
